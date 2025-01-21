bb020899a501c7f4bfafb91de105e5f288d98d1c
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-01-21 22:57:49 +0100

    Update agent1.py

--- agent1.py
+++ agent1.py
@@ -18,7 +18,7 @@ def main():
 
   agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
 
-  agent.run("How can I travel from Nigeria to Mexico transiting in Europe, and give me the cheapest round trip ticket in March?")
-
+  response = agent.run("How can I travel from Nigeria to Mexico transiting in Europe, and give me the cheapest round trip ticket in March?")
+  st.write(response)
 if __name__ == '__main__':
     main()
