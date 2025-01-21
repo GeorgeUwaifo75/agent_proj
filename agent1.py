5b7b9bae8e439c52423b7c3169afa77e18a88a20
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-01-21 15:39:11 +0100

    Update agent1.py

--- agent1.py
+++ agent1.py
@@ -2,7 +2,19 @@ import os
 import streamlit as st
 
 from dotenv import load_dotenv
+from transformers import tool
 
+@tool
+def model_download_tool(task: str) -> str:
+    """
+    This is a tool that returns the most downloaded model of a given task on the Hugging Face Hub.
+    It returns the name of the checkpoint.
+
+    Args:
+        task: The task for which
+    """
+    model = next(iter(list_models(filter="text-classification", sort="downloads", direction=-1)))
+    return model.id
 
 
 def main():
