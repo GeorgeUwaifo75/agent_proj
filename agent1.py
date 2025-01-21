55b73b4b6f3fe93cbd23c9830456add99094565a
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-01-21 15:34:58 +0100

    Update agent1.py

--- agent1.py
+++ agent1.py
@@ -8,10 +8,10 @@ from dotenv import load_dotenv
 def main():
   load_dotenv()
   api_key = os.getenv('HF_TOKEN')
-      if api_key:
-          st.write(f"API Key loaded (but not shown): *******")
-      else:
-          st.write("API Key not loaded! Ensure you have a '.env' file with API_KEY set.")
+  if api_key:
+      st.write(f"API Key loaded (but not shown): *******")
+  else:
+      st.write("API Key not loaded! Ensure you have a '.env' file with API_KEY set.")
   st.title('The Agent test')
 
 if __name__ == '__main__':
