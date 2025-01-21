bebf5e162309f03940ecedbc42da919b355e6072
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-01-21 15:41:35 +0100

    Update agent1.py

--- agent1.py
+++ agent1.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 from dotenv import load_dotenv
 from transformers import tool
+from transformers import CodeAgent, HfApiEngine
 
 @tool
 def model_download_tool(task: str) -> str:
@@ -25,6 +26,17 @@ def main():
   else:
       st.write("API Key not loaded! Ensure you have a '.env' file with API_KEY set.")
   st.title('The Agent test')
+
+
+  
+
+llm_engine = HfApiEngine(model="https://huggingface.co/inference-endpoints")
+agent = CodeAgent(tools=[], llm_engine=llm_engine, add_base_tools=True)
+
+#agent = CodeAgent(tools=[model_download_tool], llm_engine=llm_engine)
+agent.run(
+"Can you give me the name of the model that has the most downloads in the 'text-to-video' task on the Hugging Face Hub?"
+)
 
 if __name__ == '__main__':
     main()
